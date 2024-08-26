4d94aed71389a4c3a2e8ed5e394216b7c51f9307
Gabriele <gabriele.palma.bool@gmail.com>
2024-08-26 12:54:34 +0200

    add

--- Front/src/pages/Home.vue
+++ Front/src/pages/Home.vue
@@ -1,11 +1,60 @@
 <script>
 import axios from "axios";
 
+//importo store
+import { store } from "./../store";
+
 export default {
-  name: "Home",
+  components: {
+    name: "Home",
+  },
+
+  data() {
+    return {
+      store,
+    };
+  },
+
+  methods: {
+    getTech() {
+      let myURL = store.apiURL;
+
+      console.log(myURL);
+
+      //prima chiamata axios
+      axios
+        .get(myURL)
+        .then((res) => {
+          store.DayList = res.data.messages;
+
+          console.log(store.DayList);
+        })
+        .catch((err) => {
+          console.log("Errori", err);
+        });
+    },
+
+    getImageUrl(day) {
+      return `http://localhost:8000/storage/${day.immagine}`;
+    },
+  },
+
+  mounted() {
+    this.getTech();
+  },
 };
 </script>
 
-<template>okok</template>
+<template>
+  <div class="container">
+    <h1>Il mio primo viaggio</h1>
+    <div class="row row-cols-5">
+      <div v-for="(day, i) in store.DayList" :key="i" class="col p-2">
+        {{ day.titolo }}
+        <img :src="getImageUrl(day)" alt="immagine" />
+      </div>
+    </div>
+  </div>
+</template>
 
 <style lang="scss"></style>
--- Front/src/store.js
+++ Front/src/store.js
@@ -1,6 +1,6 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-  TechList: [],
+  DayList: [],
   apiURL: "http://localhost:8000/api/v1/voyager",
 });
